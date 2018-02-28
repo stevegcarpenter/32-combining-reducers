bcfac17bb2908048a27f64b4a3bdd9b65b81fc62
Steven Carpenter <stevegcarpenter@gmail.com>
2018-02-27 23:52:24 -0800

    CategoryItem redux component up and working

--- lab-steve/src/components/category/category-item/category-item.js
+++ lab-steve/src/components/category/category-item/category-item.js
@@ -3,21 +3,25 @@ import { connect } from 'react-redux';
 import { renderIf } from '../../../lib/utils';
 import CategoryForm from '../category-form/category-form';
 import { categoryUpdate, categoryDelete } from '../../../actions/category-actions';
+import { expenseCreate } from '../../../actions/expense-actions';
+import ExpenseForm from '../../expense/expense-form/expense-form';
+import ExpenseItem from '../../expense/expense-item/expense-item';
 
 class CategoryItem extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      category: this.props.category,
       editmode: false,
     };
 
-    this.handleDelete = this.handleDelete.bind(this);
-    this.handleEditMode = this.handleEditMode.bind(this);
+    // Binding Handlers
+    Object.getOwnPropertyNames(CategoryItem.prototype)
+      .filter(prop => prop.startsWith('handle'))
+      .map(prop => this[prop] = this[prop].bind(this));
   }
 
   handleDelete() {
-    this.props.categoryItemDelete(this.state.category);
+    this.props.categoryItemDelete(this.props.category);
   }
 
   handleEditMode() {
@@ -26,10 +30,11 @@ class CategoryItem extends React.Component {
 
   render() {
     return (
-      <div onDoubleClick={this.handleEditMode}>
+      <div>
         <h4>Category: {this.props.category.name}</h4>
         <p>Budget: {this.props.category.budget}</p>
         <button onClick={this.handleDelete}>Delete</button>
+        <button onClick={this.handleEditMode}>Edit</button>
         {renderIf(this.state.editmode,
           <CategoryForm
             category={this.props.category}
@@ -37,18 +42,34 @@ class CategoryItem extends React.Component {
             onComplete={this.props.categoryItemUpdate}
           />
         )}
+        <h5>Add an Expense</h5>
+        <ExpenseForm
+          buttonText='Create'
+          categoryId={this.props.category._id}
+          onComplete={this.props.expenseItemCreate}
+        />
+        {renderIf(this.props.expenses,
+          this.props.expenses[this.props.category._id].map(exp => (
+            <ExpenseItem
+              key={exp._id}
+              expense={exp}
+            />
+          ))
+        )}
       </div>
     );
   }
 }
 
 const mapStateToProps = state => ({
-  categories: state,
+  categories: state.categories,
+  expenses: state.expenses,
 });
 
 const mapDispatchToProps = (dispatch, getState) => ({
   categoryItemDelete: category => dispatch(categoryDelete(category)),
   categoryItemUpdate: category => dispatch(categoryUpdate(category)),
+  expenseItemCreate: expense => dispatch(expenseCreate(expense)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(CategoryItem);
